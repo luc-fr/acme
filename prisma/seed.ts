0e866feb4deec1bb4679c441322e35cf07e955cd
luc-fr <lucfr1991@gmail.com>
2026-03-25 15:12:09 +0000

    fix: corrigir estrutura de data para criacao de notas fiscais

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,12 +5,12 @@ import { Acme } from 'next/font/google';
 const prisma = new PrismaClient();
 
 async function main() {
-    console.log('Iniciando pupulação do banco de dados...')
+    console.log('Iniciando população do banco de dados...')
 
     const password = await bcrypt.hash('password', 10);
 
     const user = await prisma.user.upsert({
-        where: { email: 'admin@Acme.com' },
+        where: { email: 'admin@acme.com' },
         update: {},
         create: {
             name: 'Admin',
@@ -24,15 +24,15 @@ async function main() {
     const customer_data = [{
         name: 'Alex Bessa',
         email: 'alex@email.com',
-        imageUrl: 'https://ui-avatars.com/api/?name=Alex+Bessa&background=radom'
+        imageUrl: 'https://ui-avatars.com/api/?name=Alex+Bessa&background=random'
     }, {
         name: 'Valdiana Bessa',
         email: 'valdiana@email.com',
-        imageUrl: 'https://ui-avatars.com/api/?name=Valdiana+Bessa&background=radom'
+        imageUrl: 'https://ui-avatars.com/api/?name=Valdiana+Bessa&background=random'
     }, {
         name: 'Timóteo Bessa',
         email: 'timoteo@email.com',
-        imageUrl: 'https://ui-avatars.com/api/?name=Timoteo+Bessa&background=radom'
+        imageUrl: 'https://ui-avatars.com/api/?name=Timoteo+Bessa&background=random'
     }];
 
     const customers = [];
@@ -45,68 +45,68 @@ async function main() {
         });
 
         customers.push(customer);
-        console.log('Cliente criado: ${ customer.name }');
+        console.log(`Cliente criado: ${ customer.name }`);
     };
 
     const invoicesData = [{
         amount: 15785,
         status: InvoiceStatus.PENDENTE,
-        date: '2026-29-05',
+        date: '2026-05-29',
         customer: customers[0]
     }, {
         amount: 5722,
         status: InvoiceStatus.PENDENTE,
-        date: '2026-15-05',
+        date: '2026-05-15',
         customer: customers[1]
     }, {
         amount: 154225785,
         status: InvoiceStatus.PENDENTE,
-        date: '2026-12-05',
+        date: '2026-05-12',
         customer: customers[2]
     }, {
         amount: 15474785,
         status: InvoiceStatus.PENDENTE,
-        date: '2026-15-05',
+        date: '2026-05-15',
         customer: customers[0]
     }, {
         amount: 4747,
         status: InvoiceStatus.PAGO,
-        date: '2026-05-05',
+        date: '2026-05-02',
         customer: customers[1]
     }, {
         amount: 747,
         status: InvoiceStatus.PAGO,
-        date: '2026-15-05',
+        date: '2026-05-15',
         customer: customers[2]
     }, {
         amount: 7575,
         status: InvoiceStatus.PENDENTE,
-        date: '2026-16-05',
+        date: '2026-05-16',
         customer: customers[0]
     }, {
         amount: 5777,
         status: InvoiceStatus.PAGO,
-        date: '2026-03-05',
+        date: '2026-05-03',
         customer: customers[1]
     }, {
         amount: 5757,
         status: InvoiceStatus.PAGO,
-        date: '2026-01-05',
+        date: '2026-05-01',
         customer: customers[2]
     }, {
         amount: 5858,
         status: InvoiceStatus.PENDENTE,
-        date: '2026-16-05',
+        date: '2026-05-16',
         customer: customers[0]
     }, {
         amount: 5959,
         status: InvoiceStatus.PAGO,
-        date: '2026-08-05',
+        date: '2026-05-08',
         customer: customers[1]
     }, {
         amount: 1578591,
         status: InvoiceStatus.PENDENTE,
-        date: '2026-20-05',
+        date: '2026-05-20',
         customer: customers[2]
     }];
 
