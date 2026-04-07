b95aa43715db0a038fc40e2f5f63018806fda866
luc-fr <lucfr1991@gmail.com>
2026-04-07 22:41:21 +0000

    feat: definir contrato de tipos para usuarios, clientes, faturas, receita e api

---
+++ src/app/types/index.ts
@@ -0,0 +1,49 @@
+export interface User {
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+};
+
+export type CreateUserData = Omit<User, 'id'>;
+export type UpdateUserData = Partial<CreateUserData>
+
+export interface Customer {
+    id: string;
+    name: string;
+    email: string;
+    imageUrl: string;
+};
+
+export type CreateCustomerData = Omit<Customer, 'id'>;
+export type UpdateCustomerData = Partial<CreateCustomerData>
+
+export type InvoiceStatus = 'PEDENTE' | 'PAGO';
+
+export interface Invoice {
+    id: string;
+    customer_id: string;
+    amount: number;
+    data: Date;
+    status: InvoiceStatus;
+};
+
+export type CreateInvoiceData = Omit<Invoice, 'id'>;
+export type UpdateInvoiceData = Partial<CreateInvoiceData>
+
+export interface Revenue {
+    month: string;
+    revenue: number;
+};
+
+// Generics
+
+export interface ApiResponse {
+    data: T;
+    message?: string;
+};
+
+export interface ApiError {
+    error: string;
+    details?: unknown;
+};
