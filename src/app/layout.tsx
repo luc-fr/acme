d324cd1205189d6e43c5721ef5b1b78b0be59e22
luc-fr <lucfr1991@gmail.com>
2026-04-15 23:31:44 +0000

    feat: implementar controladores de rota de clientes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import { inter } from '@/ui/fonts';
+import { inter } from '@/app/ui/fonts';
 import '@/app/globals.css';
 
 export const metadata: Metadata = {
